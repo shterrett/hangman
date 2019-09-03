3098286e16fe7340dd85ca1973405faa6d37e29b
Stuart Terrett <shterrett@gmail.com>
2019-09-03 07:18:58 -0400

    Use lenses

--- src/GameState.hs
+++ src/GameState.hs
@@ -1,15 +1,17 @@
 module GameState where
 
+import Control.Lens (makeLenses)
 import Data.Text (Text)
 import Data.Set (Set)
 
 data GameState =
-    GameState { wordList :: [Text]
-              , target :: Text
-              , haystack :: Set Char
-              , correctGuesses :: Set Char
-              , incorrectGuesses :: Set Char
+    GameState { _wordList :: [Text]
+              , _target :: Text
+              , _haystack :: Set Char
+              , _correctGuesses :: Set Char
+              , _incorrectGuesses :: Set Char
               }
+makeLenses ''GameState
 
 data GuessError =
     Incorrect Char
--- src/GuessEffect.hs
+++ src/GuessEffect.hs
@@ -3,6 +3,7 @@ module GuessEffect where
 import qualified Data.Set as Set
 import Control.Effect
 import Control.Effect.State
+import Control.Lens ((^.), (%~))
 import GameState
 
 guess :: (Member (State GameState) sig, Carrier sig m)
@@ -12,31 +13,33 @@ guess c = do
     game :: GameState <- get
     case alreadyGuessed game c >>= checkGuess game of
       Right c' ->
-        put $ game { correctGuesses = Set.insert c' (correctGuesses game) }
+        put $ insertChar game c' correctGuesses
       Left (Incorrect c') ->
-        put $ game { incorrectGuesses = Set.insert c' (incorrectGuesses game) }
+        put $ insertChar game c' incorrectGuesses
       Left (AlreadyGuessed _) -> pure ()
-    pure $ either id id $ loseGame game >>= winGame
+    pure $ fromEither $ loseGame game >>= winGame
+  where fromEither = either id id
+        insertChar g ch l = (l %~ (Set.insert ch)) g
 
 alreadyGuessed :: GameState -> Char -> Either GuessError Char
 alreadyGuessed g c =
-    if Set.member c (Set.union (correctGuesses g) (incorrectGuesses g))
+    if Set.member c (Set.union (g ^. correctGuesses) (g ^. incorrectGuesses))
       then Left $ AlreadyGuessed c
       else Right c
 
 checkGuess :: GameState -> Char -> Either GuessError Char
 checkGuess g c =
-    if Set.member c (haystack g)
+    if Set.member c (g ^. haystack)
       then Right c
       else Left $ Incorrect c
 
 loseGame :: GameState -> Either GuessResult GameState
 loseGame g =
-    if Set.size (incorrectGuesses g) >= maxMisses
+    if Set.size (g ^. incorrectGuesses) >= maxMisses
       then Left Lose
       else Right g
 
 winGame :: GameState -> Either GuessResult GuessResult
-winGame g = if (correctGuesses g) == (haystack g)
+winGame g = if (g ^. correctGuesses) == (g ^. haystack)
               then Right Win
               else Left Continue
